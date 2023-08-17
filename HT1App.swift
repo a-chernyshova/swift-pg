bbef2e3b446af949960ff3f6e16cf02049082b72
Anastasia Chernyshova <stasya.aska@gmail.com>
2023-08-17 21:44:00 +0200

    some random data gen

--- HT1App.swift
+++ HT1App.swift
@@ -12,20 +12,19 @@ struct HT1App: App {
     /// Instance class CallScreenManager
     @ObservedObject
     var callScreenManager = CallScreenManager()
+//    var contactList: Array<ContactModel>
     
-    let contactList: Array<ContactModel> = [
-        ContactModel(id: UUID(), firstName: "Thomas", secondName: "Anderson", phoneNumber: "+49 (151) 630-57558"),
-        ContactModel(id: UUID(), firstName: "Darya", secondName: "Chernyshova", phoneNumber: "+7 (960) 239-9161"),
-        ContactModel(id: UUID(), firstName: "Mom", secondName: "", phoneNumber: "+7 (962) 208-9021"),
-        ContactModel(id: UUID(), firstName: "Teacher", secondName: "", phoneNumber: "+7 (921) 318-3970"),
-        ContactModel(id: UUID(), firstName: "Stiven", secondName: "Spilberg", phoneNumber: "+1 (565) 318-3970")
-    ]
-    
-    init() {
-        
-    }
+    init() {}
     
     var body: some Scene {
+        let contactList: Array<ContactModel> = generateContacts(n: 25)
+//        var contactList = [
+//            ContactModel(id: UUID(), firstName: "Thomas", secondName: "Anderson", phoneNumber: "+49 (151) 600-57777"),
+//            ContactModel(id: UUID(), firstName: "Darya", secondName: "Chernyshova", phoneNumber: "+7 (960) 222-1166"),
+//            ContactModel(id: UUID(), firstName: "Mom", secondName: "", phoneNumber: "+7 (962) 200-9001"),
+//            ContactModel(id: UUID(), firstName: "Teacher", secondName: "", phoneNumber: "+7 (921) 008-3900"),
+//            ContactModel(id: UUID(), firstName: "Stiven", secondName: "Spilberg", phoneNumber: "+1 (565) 318-3970")
+//        ]
         WindowGroup {
             ZStack{
                 ContactsScreen(contactList: contactList)
@@ -34,5 +33,26 @@ struct HT1App: App {
                 }
             }.environmentObject(callScreenManager)
         }
+    }
+    
+    func randomString(length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyz"
+        return String((0..<length).map{ _ in letters.randomElement()! }).capitalized
+    }
+                     
+    func randomDigitString(length: Int) -> String {
+        let letters = "0123456789"
+        return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+    
+    func generateContacts(n: Int) -> Array<ContactModel> {
+        var contList: Array<ContactModel> = Array()
+        for _ in 1...n {
+            contList.append(ContactModel(id: UUID(),
+                                         firstName: randomString(length: Int.random(in: 4..<6)),
+                                         secondName: randomString(length: Int.random(in: 4..<9)),
+                                         phoneNumber: "+\(Int.random(in: 0..<9)) (\(randomDigitString(length: 3))) \(randomDigitString(length: 3))-\(randomDigitString(length: 4))"))
+        }
+        return contList
     }
 }
