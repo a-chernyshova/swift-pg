da82922a7d5746478f5331f09708c0ee5536a842
Anastasia Chernyshova <stasya.aska@gmail.com>
2023-09-05 19:04:13 +0200

    Update ContactPageScreen.swift

--- HT1/Screens/ContactPageScreen.swift
+++ HT1/Screens/ContactPageScreen.swift
@@ -100,7 +100,7 @@ struct ContactPageScreen: View {
 struct ContactPageScreen_Previews: PreviewProvider {
     static var previews: some View {
         let neo = ContactModel(
-            id: UUID(), firstName: "Anastasia", secondName: "Che", phoneNumber: "+49 (151) 630-57558", imageName: "1", isAccountBlocked: false
+            id: UUID(), firstName: "Thomas", secondName: "Anders", phoneNumber: "+49 (151) 630-55555", imageName: "1", isAccountBlocked: false
         )
         
         ContactPageScreen(contactModel: neo)
